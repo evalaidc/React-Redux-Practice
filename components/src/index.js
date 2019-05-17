75fa1f27cca9f83fb05fff940bd4f30ca736b5e7
Eva Lai <evalai.dc@gmail.com>
2019-05-16 22:26:59 -0400

    practice writing multiple props and pass to child

--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from './commentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" />
-            <CommentDetail author="Alex" />
-            <CommentDetail author="Jane" />
+            <CommentDetail 
+            author="Sam" 
+            timeAgo="Today at 4:45PM" 
+            avatar={faker.image.avatar()} 
+            commentText="Nice blog post" 
+            />
+            <CommentDetail 
+            author="Alex" 
+            timeAgo="Today at 2:00PM" 
+            avatar={faker.image.avatar()} 
+            commentText="Second post"
+            />
+            <CommentDetail 
+            author="Jane" 
+            timeAgo="Yesterday at 5:00PM" 
+            avatar={faker.image.avatar()} 
+            commentText="third post"
+            />
         </div>
     );
 };
