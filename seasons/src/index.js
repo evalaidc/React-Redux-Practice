0ccd608ea4e29f35258ab067c245b337b21c63d4
Eva Lai <evalai.dc@gmail.com>
2019-06-25 16:56:33 -0400

    intiial state of season react app

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,12 +2,55 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
+    // State - JS object that contains data relevant to a component
+    // update state to get component to update its self - can only be updated using 'setState'
+    constructor(props) {
+        // pulls properties from the base class (React.Component in this case)
+        super(props);
+
+        this.state = {
+            lat: null,
+            errorMessage: ''
+        };
+
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
+            (position) => {
+                // called setState to update state and rerender component
+                this.setState({
+                    lat: position.coords.latitude
+                })
+            },
+            (err) => {
+                this.setState({
+                    errorMessage: err.message
+                })
+            }
         );
-        return <div> Latitude: </div>;
+    }
+
+    //React says we hve to define render
+    render() {
+        if(this.state.errorMessage && !this.state.lat) {
+            return (
+                <div> 
+                    Error: {this.state.errorMessage}
+                </div>
+            ); 
+        }
+
+        if( this.state.errorMessage && this.state.lat) {
+            return (
+                <div> 
+                    Latitude: {this.state.lat}
+                </div>
+            );
+        }
+
+        return (
+            <div> 
+                Loading...
+            </div>
+        );
     }
 }
 
