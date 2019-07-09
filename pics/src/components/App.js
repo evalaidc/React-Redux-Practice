de4ae19bb0d4fdc2be692ce05901324f821d27a9
Eva Lai <evalai.dc@gmail.com>
2019-07-09 11:02:12 -0400

    added initial axios response calls

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component{
-    onSearchSubmit(term) {
-        console.log(term)
+    state = { images: [] };
+
+    async onSearchSubmit(term) {
+        const response = await axios
+        .get('https://api.unsplash.com/search/photos', {
+            params: {
+                query: term
+            },
+            headers: {
+                Authorization: 'Client-ID 8376644ee9b97c77bd1af4155d1d5a07631a98cca889682bf2c57d303d8c14ae'
+            }
+        })
+
+        console.log(response.data.results)
     }
 
     render() {        
