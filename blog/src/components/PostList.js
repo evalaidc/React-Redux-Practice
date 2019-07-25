530bdee9742b7d7f53f57ae2431db863858b582a
Eva Lai <evalai.dc@gmail.com>
2019-07-25 15:38:57 -0400

    lists all blog posts

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -7,10 +7,26 @@ class PostList extends React.Component {
         this.props.fetchPosts();
     }
 
+    renderList() {
+        return this.props.posts.map(post => {
+            return (
+                <div className="item" key={post.id}>
+                    <i className="large middle aligned icon user"></i>
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                </div>
+            );
+        });
+    }
+
     render() {
         return (
-            <div>
-                Post List
+            <div className="ui relaxed divided list">
+                {this.renderList()}
             </div>
         )
     };
@@ -22,5 +38,4 @@ const mapStateToProps = (state) => {
 
 export default connect(
     mapStateToProps,
-    { fetchPosts })
-    (PostList);
+    { fetchPosts })(PostList);
