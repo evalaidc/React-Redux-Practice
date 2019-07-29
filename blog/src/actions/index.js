87fa996b0808dfac64b52c3181689d36e64b2e61
Eva Lai <evalai.dc@gmail.com>
2019-07-29 16:12:46 -0400

    created users reducer and created user name header

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,15 +1,22 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = () => {
+export const fetchPosts = () => async dispatch => {
     // dispatch - can pass actions in dispatch function - unlimited power to change data in redux - change data
     // getState - returns state of redux store - read data
     // redux thunk - allows us to return values of the inner function.
-    return async dispatch => {
-        const response = await jsonPlaceholder.get('/posts');
-    
-        dispatch({
+    const response = await jsonPlaceholder.get('/posts');
+
+    return dispatch({
             type: 'FETCH_POSTS',
             payload: response.data
         })
-    };
 };
+
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    return dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    });
+}
