3e03d1195f669f09a89f01b43cfd120a992b2e18
Eva Lai <evalai.dc@gmail.com>
2019-07-30 10:59:37 -0400

    refactored code to with fetch and lodash chain

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,16 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    _.chain(getState().posts)
+      .map('userId')
+      .uniq()
+      .forEach((id) => dispatch(fetchUser(id)))
+      .value();
+}
+
 export const fetchPosts = () => async dispatch => {
     // dispatch - can pass actions in dispatch function - unlimited power to change data in redux - change data
     // getState - returns state of redux store - read data
@@ -14,18 +24,23 @@ export const fetchPosts = () => async dispatch => {
 };
 
 // memoize - calls function only once and caches the list/request to be easily found
-export const fetchUser = (id) => dispatch => {
-    _fetchUser(id, dispatch)
-};
-
-// allows us to memoize and have the request sent one time. - cannot recall, you will have to recreate another function if you want to call the reducer again.
-// memoize allows for it one time.
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
     
     dispatch({
         type: 'FETCH_USER',
         payload: response.data
     });
+};
+
+// // allows us to memoize and have the request sent one time. - cannot recall, you will have to recreate another function if you want to call the reducer again.
+// // memoize allows for it one time.
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
     
-});
+//     dispatch({
+//         type: 'FETCH_USER',
+//         payload: response.data
+//     });
+    
+// });
